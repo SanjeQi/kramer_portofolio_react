4e14616e9f857f26f45d92d8d452dbf8cebc2539
SanjeQi <cristi.nord@gmail.com>
2021-03-03 21:22:06 +0000

    kramer: initianl setup and component play

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RegularClass {}
+class ComponentClass extends Component {}
+
+const regularClassInstance = new RegularClass();
+const componentClassInstance = new ComponentClass();
+
+console.log('regularClassInstance', regularClassInstance);
+console.log('componentClassInstance', componentClassInstance);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <p>My name is Kramer. I'm the neighbor</p>
+        <p>I'm always around Jerry's apartment.</p>
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <p>My name is Kramer. I'm the neighbor</p>
+//       <p>I'm always around Jerry's apartment.</p>
+//     </div>
+//   );
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+
+class Animal {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  speak() {
+    console.log(`I'm`, this.name, 'and I am', this.age, 'years old.');
+  }
+}
+
+const animal1 = new Animal('Simba', 29);
+animal1.speak();
+
+console.log(animal1);
+
+class Lion extends Animal {
+  constructor(name, age, furColor, speed) {
+    super(name, age);
+    this.furColor = furColor;
+    this.speed = speed;
+  }
+  roar() {
+    console.log(
+      'Roooor! My fur is: ',
+      this.furColor,
+      'color and my top speed is: ',
+      this.speed
+    );
+  }
+}
+
+const lion1 = new Lion('Mufasa', 22, 'golden', '88km/h');
+
+console.log(lion1);
+
+lion1.speak();
+lion1.roar();
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
