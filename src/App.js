96f343e635da38dc7754bd949489867b9dddf7c4
SanjeQi <cristi.nord@gmail.com>
2021-08-05 16:31:49 +0100

    State Play

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: false };
+    this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+  }
+
+  toggleDisplayBio() {
+    this.setState({ displayBio: !this.state.displayBio });
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello!</h1>
+        <p>My name is Kramer.I'm always around Jerry's apartment</p>
+        <p>I just got a haircut</p>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in New York City</p>
+            <p>I like fruit</p>
+            <p>I love my jacket</p>
+            <p>I have the Kavorka.</p>
+            <button onClick={this.toggleDisplayBio}>Show Less</button>
+          </div>
+        ) : (
+          <div>
+            <button onClick={this.toggleDisplayBio}>Read More</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
