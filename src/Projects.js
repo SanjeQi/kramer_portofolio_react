02e36b860b7e9976dbe32a4b72b09bc00db96dcd
SanjeQi <cristi.nord@gmail.com>
2021-08-11 14:02:41 +0100

    Props and Project Component

--- src/Projects.js
+++ src/Projects.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
-class Projects extends Component {
+class Project extends Component {
   render() {
+    const { title, description, image, link } = this.props.project;
     return (
-      <div>
-        <h2>Highlighted Projects</h2>
-        <div>
-          {PROJECTS.map((PROJECT) => {
-            return <div key={PROJECT.id}>{PROJECT.title}</div>;
-          })}
-        </div>
+      <div style={{ display: 'inline-block', width: 300, margin: 10 }}>
+        <h3>{title}</h3>
+        <img src={image} alt="profile" style={{ width: 200, height: 120 }} />
+        <p>{description}</p>
+        <a href={link}>See Project</a>
       </div>
     );
   }
 }
+
+class Projects extends Component {
+  render() {
+    return (
+      <div>
+        {PROJECTS.map((PROJECT) => {
+          return <Project key={PROJECT.id} project={PROJECT} />;
+        })}
+      </div>
+    );
+  }
+}
+
 export default Projects;
