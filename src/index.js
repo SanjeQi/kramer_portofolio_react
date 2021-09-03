38eaf9e1f4cd3e68f84d24b5d22fd97478bb71fa
SanjeQi <cristi.nord@gmail.com>
2021-09-03 17:55:03 +0100

    Clean up

--- src/index.js
+++ src/index.js
@@ -4,16 +4,3 @@ import App from './components/App';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-new Promise((resolve, reject) => {
-  return reject(new Error('No Bears'));
-
-  setInterval(() => {
-    console.log('Bears');
-    resolve('Bears, Beets, Battlestar Galactica');
-  }, 2000);
-})
-  .then((quote) => {
-    console.log(quote);
-  })
-  .catch((error) => console.log('error', error));
